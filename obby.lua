732e67fd20172a869b588cd866e5aa6a76c17760
saviorseven11@gmail.com <saviorseven11@gmail.com>
2021-03-31 19:55:28 -0700

    unfucked obby + vesp cred

--- obby.lua
+++ obby.lua
@@ -3,28 +3,30 @@
 
 --Dont touch!
 obbyActive = false
+currentslot = 1
 
 --Type of tool you want to use, just for netherite in the future
 --Put "minecraft:diamond_pickaxe" if you want a dimmie pick
 --Put "minecraft:netherite_pickaxe" if u want a netherite pick
-local tool = "minecraft:diamond_pickaxe"
+tool = "minecraft:diamond_pickaxe"
 
 --Specify the least amount of string a bot can have in its inventory at once
 --make sure you start with more string than this number is set to.
-local strAmount = "32"
+strAmount = 32
 
 local function mine()
 
         --Looks at dispenser and gets string
-        look(90,0)
+        look(-80,10)
         sleep(50)
         use(4000) --Holds use for amnt of miliseconds
-        sleep(50)
+        sleep(4000)
 
         --Quick string check
-        pick("minecraft:string")
-        local amount = openInventory().getSlot(getHotbar())['amount']
-        if amount > strAmount then
+        local slot = pick("minecraft:string")
+		sleep(40) --delay
+        amount = slot['amount']
+        if amount < strAmount then
             log("§5[Kallos]§8Bot Disabled")
             log("§5[Kallos]§8Not enough string!")
             obbyActive = false
@@ -35,16 +37,16 @@ local function mine()
 
         --Places String
         sneak(1300)
-        look(-90,9)
+        look(180,4)
         sleep(500)
         pick("minecraft:string")
         sleep(200)
-        use(600)
+        use(800)
         sleep(600)
 
         --Looks and opens lever, waits 4 seconds, and then closes to gen obby.
         sleep(500)
-        look(180,-30)
+        look(90,-60)
         sleep(500)
         use(nil)
         sleep(4000)
@@ -52,12 +54,12 @@ local function mine()
         sleep(500)
 
         --Mines obsidian
-        look(-90,0)
+        look(180,0)
         sleep(500)
-        pick(tool)
+        local tool = pick(tool)
         --Quick durability check
-        local dura = openInventory().getSlot(getHotbar())['maxDmg'] - openInventory().getSlot(getHotbar())['dmg']
-        if dura > 32 then
+        local dura = tool['maxDmg'] - tool['dmg']
+        if dura <= 32 then
             log("§5[Kallos]§8Bot Disabled")
             log("§5[Kallos]§8Pickaxe durability is too low.")
             obbyActive = false
@@ -68,17 +70,14 @@ local function mine()
         sleep(200)
         attack(14000)
         sleep (14000)
-end
 
---Fetches obj dura
-function durability(obj)
-    local inv = openInventory()
-    local t = inv.getSlot(findItem(obj))['maxDmg'] - inv.getSlot(findItem(obj))['dmg']
-    inv.close()
-    return t
+        while variable do
+            mine()
+        end
 end
 
 --Pick item in hotbar first, followed by inventory
+--God bless vespasian for making this helpful thing
 function pick(obj)
     local inv = openInventory()
     
@@ -110,8 +109,11 @@ function pick(obj)
     inv.close()
     end
 
---Actual Code Start
-while currentslot < 10 do
+
+variable = not variable
+if not variable then return end
+
+while variable do
     if obbyActive == false then
         log("§5[Kallos]§8Bot Enabled")
         obbyActive = true
