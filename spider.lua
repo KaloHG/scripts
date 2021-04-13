96670fa4622c46a43d8c5c39d86ddc1aa249b5b8
saviorseven11@gmail.com <saviorseven11@gmail.com>
2021-04-13 13:33:53 -0700

    daddsad

--- spider.lua
+++ spider.lua
@@ -23,6 +23,13 @@ while leftclick do
         use(6000) --Eat for 6 seconds
         sleep(6000)
     end
+    local tool = pick('minecraft:diamond_sword')
+    local dura = tool['maxDmg'] - tool['dmg']
+    if dura < 32 then
+        say("/logout")
+        log("&cSword Dura too low to continue.")
+        stopAllScripts()
+    end
 	attack(nil) --instant click
 	sleep(1000) --one click a second.
 	if lock_cam then
@@ -37,3 +44,36 @@ local total = os.clock() - start
 local minutes = math.floor(total / 60)
 local seconds = math.floor(total % 60)
 log("&cRan spider grinder for &f" .. minutes .. "m " .. seconds .. "s")
+
+--Pick item in hotbar first, followed by inventory
+--God bless vespasian for making this helpful thing
+function pick(obj)
+    local inv = openInventory()
+    
+        -- Loops through hotbar for item
+        for i = 37, 45 do
+            -- Swaps to item and returns if found
+            if inv.getSlot(i) and inv.getSlot(i).id == obj then
+                setHotbar(i - 36)
+                return inv.getSlot(i)
+            end
+        end
+        
+        -- Loops through inventory if item not found in hotbar
+        for i = 1, inv.getTotalSlots() do
+            -- Compares every item to see if it is the item we are searching for
+            if inv.getSlot(i) and inv.getSlot(i).id == obj then
+                  -- Puts the item in an empty spot on the hotbar and swaps to it
+                  for hb = 37, 45 do
+                    if not inv.getSlot(hb) then
+                         inv.click(i)
+                        sleep(50)
+                          inv.click(hb)
+                        setHotbar(hb - 36)
+                        return inv.getSlot(i)
+                    end
+                end
+            end
+        end
+    inv.close()
+end
